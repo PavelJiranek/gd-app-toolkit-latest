771ef03ecd7f3926b05c699af774998e75c51585
Pavel Jiranek <pavel.jiranek@gooddata.com>
2024-04-08 11:28:26 +0200

    chore: update ws and catalog

--- src/catalog.ts
+++ src/catalog.ts
@@ -1,5 +1,5 @@
 /* eslint-disable */
-/* THIS FILE WAS AUTO-GENERATED USING CATALOG EXPORTER; YOU SHOULD NOT EDIT THIS FILE; GENERATE TIME: 2024-04-08T09:04:37.816Z; */
+/* THIS FILE WAS AUTO-GENERATED USING CATALOG EXPORTER; YOU SHOULD NOT EDIT THIS FILE; GENERATE TIME: 2024-04-08T09:26:08.664Z; */
 // @ts-ignore ignore unused imports here if they happen (e.g. when there is no measure in the workspace)
 import { newAttribute, newMeasure, IAttribute, IMeasure, IMeasureDefinition, idRef } from "@gooddata/sdk-model";
 
@@ -64,11 +64,6 @@ export const ProductName: IAttribute = newAttribute("product_name");
  */
 export const Category_1: IAttribute = newAttribute("products.category");
 /**
- * Attribute Title: Program Group
- * Attribute ID: Program_Group
- */
-export const ProgramGroup: IAttribute = newAttribute("Program_Group");
-/**
  * Attribute Title: Region
  * Attribute ID: region
  */
@@ -77,18 +72,35 @@ export const Region: IAttribute = newAttribute("region");
  * Attribute Title: State
  * Attribute ID: state
  */
-export const State: IAttribute = newAttribute("state");
+export const State = {
+  /**
+   * Display Form Title: State
+   * Display Form ID: state
+   */
+  Default: newAttribute("state")
+  /**
+   * Display Form Title: Location
+   * Display Form ID: geo__state__location
+   */,
+  Location: newAttribute("geo__state__location"),
+};
 /**
  * Attribute Title: Type
  * Attribute ID: type
  */
 export const Type: IAttribute = newAttribute("type");
 /**
- * Metric Title: Test
- * Metric ID: test
+ * Metric Title: Revenue
+ * Metric ID: revenue
  * Metric Type: MAQL Metric
  */
-export const Test: IMeasure<IMeasureDefinition> = newMeasure(idRef("test", "measure"));
+export const Revenue: IMeasure<IMeasureDefinition> = newMeasure(idRef("revenue", "measure"));
+/**
+ * Metric Title: Sales
+ * Metric ID: sales
+ * Metric Type: MAQL Metric
+ */
+export const Sales: IMeasure<IMeasureDefinition> = newMeasure(idRef("sales", "measure"));
 /**
  * Fact Title: Budget
  * Fact ID: budget
@@ -256,48 +268,6 @@ export const Spend = {
    * Fact Aggregation: runsum
    */,
   Runsum: newMeasure(idRef("spend", "fact"), (m) => m.aggregation("runsum")),
-};
-/**
- * Fact Title: Values
- * Fact ID: Values
- */
-export const Values = {
-  /**
-   * Fact Title: Values
-   * Fact ID: Values
-   * Fact Aggregation: sum
-   */
-  Sum: newMeasure(idRef("Values", "fact"), (m) => m.aggregation("sum"))
-  /**
-   * Fact Title: Values
-   * Fact ID: Values
-   * Fact Aggregation: avg
-   */,
-  Avg: newMeasure(idRef("Values", "fact"), (m) => m.aggregation("avg"))
-  /**
-   * Fact Title: Values
-   * Fact ID: Values
-   * Fact Aggregation: min
-   */,
-  Min: newMeasure(idRef("Values", "fact"), (m) => m.aggregation("min"))
-  /**
-   * Fact Title: Values
-   * Fact ID: Values
-   * Fact Aggregation: max
-   */,
-  Max: newMeasure(idRef("Values", "fact"), (m) => m.aggregation("max"))
-  /**
-   * Fact Title: Values
-   * Fact ID: Values
-   * Fact Aggregation: median
-   */,
-  Median: newMeasure(idRef("Values", "fact"), (m) => m.aggregation("median"))
-  /**
-   * Fact Title: Values
-   * Fact ID: Values
-   * Fact Aggregation: runsum
-   */,
-  Runsum: newMeasure(idRef("Values", "fact"), (m) => m.aggregation("runsum")),
 };
 /** Available Date Data Sets */
 export const DateDatasets = {
@@ -503,348 +473,29 @@ export const DateDatasets = {
        */,
       Default: newAttribute("date.year"),
     },
-  }
-  /**
-   * Date Data Set Title: Date_period
-   * Date Data Set ID: date_period
-   */,
-  DatePeriod: {
-    ref: idRef("date_period", "dataSet"),
-    identifier: "date_period"
-    /**
-     * Date Attribute: Date_period - Date
-     * Date Attribute ID: date_period.day
-     */,
-    DatePeriodDate: {
-      ref: idRef("date_period.day", "attribute"),
-      identifier: "date_period.day"
-      /**
-       * Display Form Title: Date_period - Date
-       * Display Form ID: date_period.day
-       */,
-      Default: newAttribute("date_period.day"),
-    }
-    /**
-     * Date Attribute: Date_period - Month/Year
-     * Date Attribute ID: date_period.month
-     */,
-    DatePeriodMonthYear: {
-      ref: idRef("date_period.month", "attribute"),
-      identifier: "date_period.month"
-      /**
-       * Display Form Title: Date_period - Month/Year
-       * Display Form ID: date_period.month
-       */,
-      Default: newAttribute("date_period.month"),
-    }
-    /**
-     * Date Attribute: Date_period - Quarter/Year
-     * Date Attribute ID: date_period.quarter
-     */,
-    DatePeriodQuarterYear: {
-      ref: idRef("date_period.quarter", "attribute"),
-      identifier: "date_period.quarter"
-      /**
-       * Display Form Title: Date_period - Quarter/Year
-       * Display Form ID: date_period.quarter
-       */,
-      Default: newAttribute("date_period.quarter"),
-    }
-    /**
-     * Date Attribute: Date_period - Week/Year
-     * Date Attribute ID: date_period.week
-     */,
-    DatePeriodWeekYear: {
-      ref: idRef("date_period.week", "attribute"),
-      identifier: "date_period.week"
-      /**
-       * Display Form Title: Date_period - Week/Year
-       * Display Form ID: date_period.week
-       */,
-      Default: newAttribute("date_period.week"),
-    }
-    /**
-     * Date Attribute: Date_period - Year
-     * Date Attribute ID: date_period.year
-     */,
-    DatePeriodYear: {
-      ref: idRef("date_period.year", "attribute"),
-      identifier: "date_period.year"
-      /**
-       * Display Form Title: Date_period - Year
-       * Display Form ID: date_period.year
-       */,
-      Default: newAttribute("date_period.year"),
-    },
-  }
-  /**
-   * Date Data Set Title: End_date
-   * Date Data Set ID: end_date
-   */,
-  EndDate: {
-    ref: idRef("end_date", "dataSet"),
-    identifier: "end_date"
-    /**
-     * Date Attribute: End_date - Date
-     * Date Attribute ID: end_date.day
-     */,
-    EndDateDate: {
-      ref: idRef("end_date.day", "attribute"),
-      identifier: "end_date.day"
-      /**
-       * Display Form Title: End_date - Date
-       * Display Form ID: end_date.day
-       */,
-      Default: newAttribute("end_date.day"),
-    }
-    /**
-     * Date Attribute: End_date - Month/Year
-     * Date Attribute ID: end_date.month
-     */,
-    EndDateMonthYear: {
-      ref: idRef("end_date.month", "attribute"),
-      identifier: "end_date.month"
-      /**
-       * Display Form Title: End_date - Month/Year
-       * Display Form ID: end_date.month
-       */,
-      Default: newAttribute("end_date.month"),
-    }
-    /**
-     * Date Attribute: End_date - Quarter/Year
-     * Date Attribute ID: end_date.quarter
-     */,
-    EndDateQuarterYear: {
-      ref: idRef("end_date.quarter", "attribute"),
-      identifier: "end_date.quarter"
-      /**
-       * Display Form Title: End_date - Quarter/Year
-       * Display Form ID: end_date.quarter
-       */,
-      Default: newAttribute("end_date.quarter"),
-    }
-    /**
-     * Date Attribute: End_date - Week/Year
-     * Date Attribute ID: end_date.week
-     */,
-    EndDateWeekYear: {
-      ref: idRef("end_date.week", "attribute"),
-      identifier: "end_date.week"
-      /**
-       * Display Form Title: End_date - Week/Year
-       * Display Form ID: end_date.week
-       */,
-      Default: newAttribute("end_date.week"),
-    }
-    /**
-     * Date Attribute: End_date - Year
-     * Date Attribute ID: end_date.year
-     */,
-    EndDateYear: {
-      ref: idRef("end_date.year", "attribute"),
-      identifier: "end_date.year"
-      /**
-       * Display Form Title: End_date - Year
-       * Display Form ID: end_date.year
-       */,
-      Default: newAttribute("end_date.year"),
-    },
-  }
-  /**
-   * Date Data Set Title: Start_date
-   * Date Data Set ID: start_date
-   */,
-  StartDate: {
-    ref: idRef("start_date", "dataSet"),
-    identifier: "start_date"
-    /**
-     * Date Attribute: Start_date - Date
-     * Date Attribute ID: start_date.day
-     */,
-    StartDateDate: {
-      ref: idRef("start_date.day", "attribute"),
-      identifier: "start_date.day"
-      /**
-       * Display Form Title: Start_date - Date
-       * Display Form ID: start_date.day
-       */,
-      Default: newAttribute("start_date.day"),
-    }
-    /**
-     * Date Attribute: Start_date - Month/Year
-     * Date Attribute ID: start_date.month
-     */,
-    StartDateMonthYear: {
-      ref: idRef("start_date.month", "attribute"),
-      identifier: "start_date.month"
-      /**
-       * Display Form Title: Start_date - Month/Year
-       * Display Form ID: start_date.month
-       */,
-      Default: newAttribute("start_date.month"),
-    }
-    /**
-     * Date Attribute: Start_date - Quarter/Year
-     * Date Attribute ID: start_date.quarter
-     */,
-    StartDateQuarterYear: {
-      ref: idRef("start_date.quarter", "attribute"),
-      identifier: "start_date.quarter"
-      /**
-       * Display Form Title: Start_date - Quarter/Year
-       * Display Form ID: start_date.quarter
-       */,
-      Default: newAttribute("start_date.quarter"),
-    }
-    /**
-     * Date Attribute: Start_date - Week/Year
-     * Date Attribute ID: start_date.week
-     */,
-    StartDateWeekYear: {
-      ref: idRef("start_date.week", "attribute"),
-      identifier: "start_date.week"
-      /**
-       * Display Form Title: Start_date - Week/Year
-       * Display Form ID: start_date.week
-       */,
-      Default: newAttribute("start_date.week"),
-    }
-    /**
-     * Date Attribute: Start_date - Year
-     * Date Attribute ID: start_date.year
-     */,
-    StartDateYear: {
-      ref: idRef("start_date.year", "attribute"),
-      identifier: "start_date.year"
-      /**
-       * Display Form Title: Start_date - Year
-       * Display Form ID: start_date.year
-       */,
-      Default: newAttribute("start_date.year"),
-    },
   },
 };
 export const Insights = {
   /**
-   * Insight Title: Budget per Category
-   * Insight ID: b2a3bef9-08af-468d-88db-0c360c6419d9
+   * Insight Title: Table
+   * Insight ID: 68dba711-161e-4365-8ba3-8f71f8bdea9b
    */
-  BudgetPerCategory: "b2a3bef9-08af-468d-88db-0c360c6419d9"
+  Table: "68dba711-161e-4365-8ba3-8f71f8bdea9b"
   /**
-   * Insight Title: No. of Customers
-   * Insight ID: b3fa5743-a237-4806-912c-442beddf281f
+   * Insight Title: Budget pie
+   * Insight ID: 6a5818db-cf93-4242-89d4-a42efb2ab6ae
    */,
-  NoOfCustomers: "b3fa5743-a237-4806-912c-442beddf281f"
+  BudgetPie: "6a5818db-cf93-4242-89d4-a42efb2ab6ae"
   /**
-   * Insight Title: Billing Calendar
-   * Insight ID: c61e6388-2143-46e5-b012-dae47c811d67
+   * Insight Title: Campaing cost by category
+   * Insight ID: d360e59b-0764-4445-9fd3-88cc5eedd3d6
    */,
-  BillingCalendar: "c61e6388-2143-46e5-b012-dae47c811d67",
+  CampaingCostByCategory: "d360e59b-0764-4445-9fd3-88cc5eedd3d6",
 };
 export const Dashboards = {
   /**
-   * Dashboard Title: SVG Image Chart Plugin
-   * Dashboard ID: 09a6d0c4-1fc5-4c00-9964-a990cc4b19b9
-   */
-  SVGImageChartPlugin: "09a6d0c4-1fc5-4c00-9964-a990cc4b19b9"
-  /**
-   * Dashboard Title: Table with image
-   * Dashboard ID: 21083898-fbf0-4d88-bebb-18e65969e46f
-   */,
-  TableWithImage: "21083898-fbf0-4d88-bebb-18e65969e46f"
-  /**
-   * Dashboard Title: Gauge Chart
-   * Dashboard ID: 28fad0a6-bebf-4b79-a9cd-fa69fb6bde03
-   */,
-  GaugeChart: "28fad0a6-bebf-4b79-a9cd-fa69fb6bde03"
-  /**
-   * Dashboard Title: Testing dashboard - no plugin
-   * Dashboard ID: 2d04010e-5795-4970-a638-4d2e3961ee90
-   */,
-  TestingDashboardNoPlugin: "2d04010e-5795-4970-a638-4d2e3961ee90"
-  /**
-   * Dashboard Title: Table PDF Export
-   * Dashboard ID: 2e813e3b-1aad-4a29-9993-d2c098ba617d
-   */,
-  TablePDFExport: "2e813e3b-1aad-4a29-9993-d2c098ba617d"
-  /**
-   * Dashboard Title: Whitelabelling Bar Plugin
-   * Dashboard ID: 373206af-3a7b-4dd3-8e49-e73e25fdf230
-   */,
-  WhitelabellingBarPlugin: "373206af-3a7b-4dd3-8e49-e73e25fdf230"
-  /**
-   * Dashboard Title: Headline bar chart
-   * Dashboard ID: 38c3e114-d36c-4310-b945-ce6dfd5df352
-   */,
-  HeadlineBarChart: "38c3e114-d36c-4310-b945-ce6dfd5df352"
-  /**
-   * Dashboard Title: Search Filter
-   * Dashboard ID: 3ace6ac4-ac43-43c1-8c78-d8204c137ebc
-   */,
-  SearchFilter: "3ace6ac4-ac43-43c1-8c78-d8204c137ebc"
-  /**
-   * Dashboard Title: Insight to action plugin
-   * Dashboard ID: 40e5b6ce-d3c0-4889-931f-7977b7214f76
-   */,
-  InsightToActionPlugin: "40e5b6ce-d3c0-4889-931f-7977b7214f76"
-  /**
-   * Dashboard Title: Collapsible sidebar
-   * Dashboard ID: 5532a726-b46a-469c-99f5-a6666bf9efd0
-   */,
-  CollapsibleSidebar: "5532a726-b46a-469c-99f5-a6666bf9efd0"
-  /**
    * Dashboard Title: Overview
-   * Dashboard ID: 5aaca311-c1d6-4e8b-b5fc-3822f3658485
-   */,
-  Overview: "5aaca311-c1d6-4e8b-b5fc-3822f3658485"
-  /**
-   * Dashboard Title: Radial Bar Chart Plugin
-   * Dashboard ID: 6f395aba-f417-42c4-b223-41dbb0572b2c
-   */,
-  RadialBarChartPlugin: "6f395aba-f417-42c4-b223-41dbb0572b2c"
-  /**
-   * Dashboard Title: Insight groups plugin
-   * Dashboard ID: 7c42f979-ce57-41fe-9bff-2e84fb9664ff
-   */,
-  InsightGroupsPlugin: "7c42f979-ce57-41fe-9bff-2e84fb9664ff"
-  /**
-   * Dashboard Title: Target Gauge KPI
-   * Dashboard ID: 87f22478-d1bd-45ac-8dc2-a9df632a0949
-   */,
-  TargetGaugeKPI: "87f22478-d1bd-45ac-8dc2-a9df632a0949"
-  /**
-   * Dashboard Title: Polar Area Chart Plugin
-   * Dashboard ID: 96d792ea-d739-4685-a9a4-d029f2f873f2
-   */,
-  PolarAreaChartPlugin: "96d792ea-d739-4685-a9a4-d029f2f873f2"
-  /**
-   * Dashboard Title: Multiple plugins dashboard
-   * Dashboard ID: ac2a59ec-a029-40d7-8ce6-964e13909daa
-   */,
-  MultiplePluginsDashboard: "ac2a59ec-a029-40d7-8ce6-964e13909daa"
-  /**
-   * Dashboard Title: WIP: Gantt Chart
-   * Dashboard ID: b345f797-dba3-4178-8da2-fa0f7c877ca8
-   */,
-  WIPGanttChart: "b345f797-dba3-4178-8da2-fa0f7c877ca8"
-  /**
-   * Dashboard Title: No Comparison Headline plugin
-   * Dashboard ID: cb7792d0-80b1-45b8-96e7-194dcf8095c8
-   */,
-  NoComparisonHeadlinePlugin: "cb7792d0-80b1-45b8-96e7-194dcf8095c8"
-  /**
-   * Dashboard Title: Table with text wrap
-   * Dashboard ID: d07fee70-f6ac-43f8-bd7b-47acaf2ee5c1
-   */,
-  TableWithTextWrap: "d07fee70-f6ac-43f8-bd7b-47acaf2ee5c1"
-  /**
-   * Dashboard Title: Dashboard Description plugin
-   * Dashboard ID: e41cfa23-3020-4609-a45e-cdf34ac0ca0a
-   */,
-  DashboardDescriptionPlugin: "e41cfa23-3020-4609-a45e-cdf34ac0ca0a"
-  /**
-   * Dashboard Title: Markdown description plugin
-   * Dashboard ID: ebe54214-778f-4736-b464-6982e878e0fb
-   */,
-  MarkdownDescriptionPlugin: "ebe54214-778f-4736-b464-6982e878e0fb",
+   * Dashboard ID: 6e43ea4d-fc64-4af6-aa59-5b735f4167ac
+   */
+  Overview: "6e43ea4d-fc64-4af6-aa59-5b735f4167ac",
 };
